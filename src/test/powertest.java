ffd87e8f0acf291cff9d08e9e2c39952d2216de4
Shubham Kumar <41825906+Shubham-Kumar-2000@users.noreply.github.com>
2022-01-30 21:24:05 +0530

    Fix Test cases name

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -5,22 +5,22 @@ import org.junit.Test;
 
 public class PowerFinderTest {
     @Test
-    public void test1Power1Is1() {
+    public void shouldReturn1When1IsRaisedToPower1() {
         assert PowerFinder.getPower(1,1) == 1;
     }
 
     @Test
-    public void test2Power1Is2() {
+    public void shouldReturn2When2IsRaisedToPower1() {
         assert PowerFinder.getPower(2,1) == 2;
     }
 
     @Test
-    public void test2Power2Is4() {
+    public void shouldReturn4When2IsRaisedToPower2() {
         assert PowerFinder.getPower(2, 2) == 4;
     }
 
     @Test
-    public void test3Power2Is9() {
+    public void shouldReturn9When3IsRaisedToPower2() {
         assert PowerFinder.getPower(3, 2) == 3*3;
     }
 }
