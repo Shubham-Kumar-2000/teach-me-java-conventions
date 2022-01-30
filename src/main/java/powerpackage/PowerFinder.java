b536255793dd0e85cff9390dfea9acef88d85f55
Shubham Kumar <41825906+Shubham-Kumar-2000@users.noreply.github.com>
2022-01-30 21:29:12 +0530

    Fix variable names

--- src/main/java/powerpackage/PowerFinder.java
+++ src/main/java/powerpackage/PowerFinder.java
@@ -1,11 +1,11 @@
 package powerpackage;
 
 public class PowerFinder {
-    public static int getPower (int xyz, int pqr) {
-        int p = 1;
-        for (int i = 0; i<pqr; i++) {
-            p *= xyz;
+    public static int getPower (int base, int power) {
+        int result = 1;
+        for (int i = 0; i<power; i++) {
+            result *= base;
         }
-        return p;
+        return result;
     }
 }
